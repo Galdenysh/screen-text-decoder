60c487ce687530496b934ff26a42d7bf040c8799
Igor Petrov <galdenysh@air-igor.jugru.org>
2022-12-20 18:09:20 +0300

    feat: init client

---
+++ client/src/components/app/app.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from "react";
+
+const App: FC = () => {
+  const text = "Hello";
+
+  return (
+    <main>
+      <div>
+        <p>
+          {text}
+        </p>
+      </div>
+    </main>
+  );
+};
+
+export default App;
--- src/app.config.ts
+++ src/app.config.ts
@@ -4,5 +4,5 @@ dotenv.config();
 
 export const PORT = process.env.PORT ?? "4000";
 export const API_KEY = process.env.API_KEY ?? "AQVNwTsdi0JYhHIDz5PAJ61xIaTyPsvIkGiMzgPV";
-export const API_URL = process.env.API_URL ?? "https://vision.api.cloud.yandex.net/vision/v1/batchAnalyze";
+export const API_VISION_URL = process.env.API_VISION_URL ?? "https://vision.api.cloud.yandex.net/vision/v1/batchAnalyze";
 export const FOLDER_ID = process.env.FOLDER_ID ?? "b1gtbmo4ht47ancvvs9t";
